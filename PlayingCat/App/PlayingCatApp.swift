fafef961a07f0f7783e9b5d1f0e04b71215e059f
Andrew <aviator67x@gmail.com>
2026-04-21 18:59:37 +0200

    original commit

---
+++ PlayingCat/App/PlayingCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCatApp.swift
+//  PlayingCat
+//
+//  Created by Andrew Kasilov on 21.04.26.
+//
+
+import SwiftUI
+
+@main
+struct PlayingCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
